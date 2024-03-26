708a7c6f73f3147f9b65b3272e773683d00dfb32
Matvey <t89109753300@gmail.com>
2024-03-26 17:24:35 +0300

    init commit

---
+++ src/main/java/com/example/prodolymp/ProdOlympApplication.java
@@ -0,0 +1,13 @@
+package com.example.prodolymp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdOlympApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdOlympApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prodolymp/ProdOlympApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prodolymp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdOlympApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
