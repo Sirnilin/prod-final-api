1684717818888a71c13dec9bf777a25ce6e9bdd3
Sirnilin <t89109753300@gmail.com>
2024-03-27 20:11:56 +0300

    feat: spec swagger, dependency admin panel

--- src/main/java/com/example/prodolymp/ProdOlympApplication.java
+++ src/main/java/com/example/prodolymp/ProdOlympApplication.java
@@ -1,8 +1,9 @@
 package com.example.prodolymp;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAdminServer
 @SpringBootApplication
 public class ProdOlympApplication {
 
