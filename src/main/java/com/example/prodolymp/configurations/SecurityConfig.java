249509510b8b478c03f5b827cfd9e1f4cd126a77
Matvey <t89109753300@gmail.com>
2024-03-30 19:50:59 +0300

    feat: swagger in /api.html

--- src/main/java/com/example/prodolymp/configurations/SecurityConfig.java
+++ src/main/java/com/example/prodolymp/configurations/SecurityConfig.java
@@ -10,12 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
-import lombok.Value;
+
 
 @Configuration
 @EnableWebSecurity
