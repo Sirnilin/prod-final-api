d43155b917d75e28fbbe7ca2acd712de5d3f966d
Matvey <t89109753300@gmail.com>
2024-03-31 23:19:29 +0300

    feat: fix cors

--- src/main/java/com/example/prodolymp/configurations/WebConfig.java
+++ src/main/java/com/example/prodolymp/configurations/WebConfig.java
@@ -2,11 +2,13 @@ package com.example.prodolymp.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -20,10 +22,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResolver(new PathResourceResolver());
     }
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://https://api.prod.webtm.ru")
+                .allowedMethods("PUT", "DELETE")
+                .allowedHeaders("header1", "header2", "header3")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(false).maxAge(3600);
     }
-    //d
+
 }
