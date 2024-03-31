79e0dc7972698b4480e4a5c7491bd8b7f3f0cca8
Andrey Danilov <galagtik10@gmail.com>
2024-04-01 01:23:49 +0300

    feat: enable cors

--- src/main/java/com/example/prodolymp/controllers/PingController.java
+++ src/main/java/com/example/prodolymp/controllers/PingController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ping")
+@CrossOrigin(origins = "*")
 public class PingController {
 
     @Operation(summary = "Просто пинг", description = "Проверка на работоспособность сервера")
